133ac860fdb784b5b60df12dd8404ccc6f6b731b
Mike Cartmell <michael.cartmell@grab.com>
2019-06-03 02:58:33 +0000

    add user_id

--- start.sh
+++ start.sh
@@ -11,7 +11,7 @@ docker-compose ps
 
 # Create test bookings
 DDB_PUT="$DDB put-item --table-name bootcamp_bookings --region ap-southeast-1 --endpoint-url http://storage:8000"
-$DDB_PUT --item '{"BookingID": {"S": "test-booking-1"}, "Location": {"M": {"Latitude": {"N":"1.3521"}, "Longitude":{"N":"103.8198"}}}, "AgentID":{"S": "test-agent-1"}, "State":{"S": "NEW"}}'
-$DDB_PUT --item '{"BookingID": {"S": "test-booking-2"}, "Location": {"M": {"Latitude": {"N":"1.3521"}, "Longitude":{"N":"103.8198"}}}, "AgentID":{"S": "test-agent-2"}, "State":{"S": "IN_PROGRESS"}}'
-$DDB_PUT --item '{"BookingID": {"S": "test-booking-3"}, "Location": {"M": {"Latitude": {"N":"1.3521"}, "Longitude":{"N":"103.8198"}}}, "AgentID":{"S": "test-agent-3"}, "State":{"S": "COMPLETED"}}'
+$DDB_PUT --item '{"BookingID": {"S": "test-booking-1"}, "Location": {"M": {"Latitude": {"N":"1.3521"}, "Longitude":{"N":"103.8198"}}}, "AgentID":{"S": "test-agent-1"}, "UserID":{"S": "user-id-1"}, "State":{"S": "NEW"}}'
+$DDB_PUT --item '{"BookingID": {"S": "test-booking-2"}, "Location": {"M": {"Latitude": {"N":"1.3521"}, "Longitude":{"N":"103.8198"}}}, "AgentID":{"S": "test-agent-2"}, "UserID":{"S": "user-id-1"}, "State":{"S": "IN_PROGRESS"}}'
+$DDB_PUT --item '{"BookingID": {"S": "test-booking-3"}, "Location": {"M": {"Latitude": {"N":"1.3521"}, "Longitude":{"N":"103.8198"}}}, "AgentID":{"S": "test-agent-3"}, "UserID":{"S": "user-id-1"}, "State":{"S": "COMPLETED"}}'
 echo 'Started the required containers, to stop run "./stop.sh"'
