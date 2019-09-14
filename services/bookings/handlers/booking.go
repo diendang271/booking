b55de189dc9e53c3d3e6a66ef52fcf5ff9d5c998
testaccount <tech.learning.ext13@grabtaxi.com>
2019-09-14 15:18:08 +0700

    Implement create booking endpoint

--- services/bookings/handlers/booking.go
+++ services/bookings/handlers/booking.go
@@ -3,12 +3,16 @@ package handlers
 
 import (
 	"context"
-
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/go-log/log"
 	"github.com/micro/go-micro/client"
 	agents_pb "gitlab.com/tech.learning.ext13/bootcamp-skeleton/services/agents/pb"
 	"gitlab.com/tech.learning.ext13/bootcamp-skeleton/services/bookings/models"
 	pb "gitlab.com/tech.learning.ext13/bootcamp-skeleton/services/bookings/pb"
+	"os"
 )
 
 type AgentService interface {
@@ -28,8 +32,47 @@ func New(store models.Storer, agentService agents_pb.AgentService) *BookingServi
 	}
 }
 
-// TODO: Implement handlers here
 func (s *BookingService) CreateBooking(ctx context.Context, req *pb.CreateBookingRequest, rsp *pb.CreateBookingResponse) error {
 	log.Log("Received BookingService.CreateBooking request")
+
+	agentReq := agents_pb.NearbyAgentRequest{
+		Location: &agents_pb.Location{
+			Latitude:  req.Location.Latitude,
+			Longitude: req.Location.Longitude,
+		}}
+	agentResp, err := s.agents.NearbyAgent(ctx, &agentReq, client.WithAddress("agent-service:9091"))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	ddb := newDDB()
+	store := models.NewStore(ddb)
+	booking := models.NewBooking()
+	booking.Location = models.Location{Latitude: req.Location.Latitude, Longitude: req.Location.Longitude}
+	booking.AgentID = agentResp.Agent.GetAgentId()
+	booking.State = models.BookingState_InProgress
+
+	err = store.Save(booking)
+	if err != nil {
+		fmt.Println(err)
+	}
 	return nil
+}
+
+func newDDB() *dynamodb.DynamoDB {
+	ddbURL := os.Getenv("DYNAMO_ENDPOINT")
+	if ddbURL == "" {
+		ddbURL = "http://localhost:8000"
+	}
+	log.Logf("using DDB endpoint %v", ddbURL)
+
+	sess, err := session.NewSession(&aws.Config{
+		Region:   aws.String("ap-southeast-1"),
+		Endpoint: aws.String(ddbURL),
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	return dynamodb.New(sess)
 }
